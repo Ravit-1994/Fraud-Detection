aa6b5f156f2960f6d91de27599589fa0580f2a74
Ravit-1994 <166744747+Ravit-1994@users.noreply.github.com>
2025-01-25 15:36:54 +0530

    Create data_loader.py

---
+++ src/data/data_loader.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def load_raw_data(filepath):
+    return pd.read_csv(filepath)
+
+def load_processed_data(filepath):
+    return pd.read_csv(filepath)
+
+# The filepath changes as per the data source
